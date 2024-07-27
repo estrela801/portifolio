e3bf4b31f1100af30199891f1bd5ec0574e02e1f
estrela801 <rohenri0808@gmail.com>
2024-07-27 19:42:04 -0300

    refactor: melhorando a lógica da animação

--- scripts/AnimationTecs.js
+++ scripts/AnimationTecs.js
@@ -1,28 +1,34 @@
 export default function AnimationTecs () {
 // document.body.style.backgroundColor='red'
-const details = {}
-// function getElement(){
-//     const elements = document.querySelectorAll('.tec')
-//     elements.forEach(el=>{
-//         const foiEsse = el
-//         return foiEsse
-//     })
-// }
-// function getDetails(){
-    
-// }
-// function createContainer(){
-
-// }
-// function addDetailsToContainer(){
-
-// }
-// function ExecuteDetails(){
-//     const esse = getElement()
-//     console.log(esse);
-//     const teste = ()=>{console.log('teste');}
-//     teste()
+const details = [
+    {"javascript":"é uma linguagem de programação amplamente utilizada no front-end."}
+]
+function getElement(){
+    const elements = document.querySelectorAll('.tec')
+    elements.forEach(el=>{
+        const foiEsse = el.innerHTML
+        return foiEsse
+    })
 }
-document.addEventListener("DOMContentLoaded", ExecuteDetails )
+function getDetails(value){
+    const elemento = value
+    const detail = details.pop(elemento)
+    return detail
+}
+function addDetailsToContainer(det){
+    const detail = det
+    const container = document.querySelector('.tec-details')
+    container.innerHTML=detail
+    const titulo = document.querySelector('.nome-tec-titulo')
+    titulo.innerHTML=chaveDoArray
+}
+function ExecuteDetails(){
+    const esse = getElement()
+    console.log(esse);
+    const teste = ()=>{console.log('teste');}
+    teste()
+}
+}
+document.addEventListener("mouseover", ExecuteDetails )
 
 
