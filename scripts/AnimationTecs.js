d63e0829ce3c2f953a540addc2d583f76a17b292
estrela801 <rohenri0808@gmail.com>
2024-07-29 18:51:03 -0300

    feat: tentandom elhorar a lógica da animação

--- scripts/AnimationTecs.js
+++ scripts/AnimationTecs.js
@@ -1,34 +1,45 @@
-export default function AnimationTecs () {
-// document.body.style.backgroundColor='red'
-const details = [
-    {"javascript":"é uma linguagem de programação amplamente utilizada no front-end."}
-]
-function getElement(){
-    const elements = document.querySelectorAll('.tec')
-    elements.forEach(el=>{
-        const foiEsse = el.innerHTML
-        return foiEsse
-    })
-}
-function getDetails(value){
-    const elemento = value
-    const detail = details.pop(elemento)
-    return detail
-}
-function addDetailsToContainer(det){
-    const detail = det
-    const container = document.querySelector('.tec-details')
-    container.innerHTML=detail
-    const titulo = document.querySelector('.nome-tec-titulo')
-    titulo.innerHTML=chaveDoArray
-}
-function ExecuteDetails(){
-    const esse = getElement()
-    console.log(esse);
-    const teste = ()=>{console.log('teste');}
-    teste()
-}
-}
-document.addEventListener("mouseover", ExecuteDetails )
+// const details = [
+//     {"javascript":"JavaScript é usado para adicionar interatividade a páginas web, desenvolver interfaces dinâmicas de front-end com frameworks como React, Angular e Vue.js, e criar back-ends com Node.js. Além disso, é utilizado no desenvolvimento de aplicativos móveis com React Native, criação de jogos web com bibliotecas como Phaser e Three.js, automação de tarefas de desenvolvimento com ferramentas como Gulp e Webpack, desenvolvimento de aplicativos de desktop com Electron e em projetos de Internet das Coisas (IoT)."}
+// ]
+// function getDetails(value, el){
 
+//     // const el = el
+//     el.innerHTML=value
+    
+//     return 
+// }
+// function addDetailsToContainer(det){
+//     const detail = det
+//     const container = document.querySelector('.tec-details')
+//     container.innerHTML=detail
+//     const titulo = document.querySelector('.nome-tec-titulo')
+//     titulo.innerHTML=chaveDoArray
+// }
 
+// function getElement(){
+//     const elements = document.querySelectorAll('.tec')
+//     let foiEsse
+//     elements.forEach(el=>{
+//          foiEsse = el
+//          console.log(el);
+//         })
+//         return foiEsse
+// }
+// const containerTec = getElement()
+// console.log(containerTec + ' log container');
+
+// containerTec.addEventListener("mouseover", )
+
+function a (){
+
+}
+
+const elementos = document.querySelector('.tec')
+const elementosChilds = elementos.children
+for (let i = 0; i < elementosChilds.length; i++) {
+    console.log(childElements[i]);
+}
+console.log(elementosChilds);
+// elementos.forEach(elemento => {
+//     elemento.addEventListener('mouseover', ()=>console.log(elemento))
+// });
