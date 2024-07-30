dedd748fe8f3fa7f0ed5648501279aab80e56a36
estrela801 <rohenri0808@gmail.com>
2024-07-30 14:52:40 -0300

    feat: finalmente terninando a função da animação

--- scripts/AnimationTecs.js
+++ scripts/AnimationTecs.js
@@ -1,45 +1,36 @@
-// const details = [
-//     {"javascript":"JavaScript é usado para adicionar interatividade a páginas web, desenvolver interfaces dinâmicas de front-end com frameworks como React, Angular e Vue.js, e criar back-ends com Node.js. Além disso, é utilizado no desenvolvimento de aplicativos móveis com React Native, criação de jogos web com bibliotecas como Phaser e Three.js, automação de tarefas de desenvolvimento com ferramentas como Gulp e Webpack, desenvolvimento de aplicativos de desktop com Electron e em projetos de Internet das Coisas (IoT)."}
-// ]
-// function getDetails(value, el){
+const details = {
+    javascript: "JavaScript é usado para adicionar interatividade a páginas web, desenvolver interfaces dinâmicas de front-end, criar back-ends com Node.js, desenvolver aplicativos móveis com React Native, criar jogos web, automatizar tarefas de desenvolvimento, desenvolver aplicativos de desktop com Electron e em projetos de IoT.",
+    html: "HTML é a linguagem de marcação usada para criar a estrutura das páginas web. Ele permite definir elementos como títulos, parágrafos, links, imagens, entre outros.",
+    css: "CSS é a linguagem de estilos utilizada para definir a apresentação de documentos HTML. Com CSS, é possível alterar cores, fontes, espaçamento e layout das páginas web.",
+    'react.js': "React é uma biblioteca JavaScript para a construção de interfaces de usuário. Ele facilita a criação de componentes reutilizáveis e gerenciamento de estado.",
+    git: "Git é um sistema de controle de versão distribuído usado para rastrear mudanças no código-fonte durante o desenvolvimento de software.",
+    "node.js": "Node.js é um ambiente de execução JavaScript que permite o uso do JavaScript no lado do servidor para criar aplicativos web escaláveis e eficientes.",
+    figma: "Figma é uma ferramenta de design gráfico baseada em nuvem usada para criar interfaces de usuário, protótipos e colaboração em tempo real.",
+    python: "Python é uma linguagem de programação versátil e popular, conhecida por sua simplicidade e legibilidade, amplamente usada em desenvolvimento web, ciência de dados, inteligência artificial e automação."
+};
 
-//     // const el = el
-//     el.innerHTML=value
-    
-//     return 
-// }
-// function addDetailsToContainer(det){
-//     const detail = det
-//     const container = document.querySelector('.tec-details')
-//     container.innerHTML=detail
-//     const titulo = document.querySelector('.nome-tec-titulo')
-//     titulo.innerHTML=chaveDoArray
-// }
-
-// function getElement(){
-//     const elements = document.querySelectorAll('.tec')
-//     let foiEsse
-//     elements.forEach(el=>{
-//          foiEsse = el
-//          console.log(el);
-//         })
-//         return foiEsse
-// }
-// const containerTec = getElement()
-// console.log(containerTec + ' log container');
-
-// containerTec.addEventListener("mouseover", )
-
-function a (){
-
+function addClass(el){
+    el.classList.add('appear')
+    setTimeout(()=>{
+        el.classList.remove('appear')
+    },350)
 }
 
-const elementos = document.querySelector('.tec')
-const elementosChilds = elementos.children
-for (let i = 0; i < elementosChilds.length; i++) {
-    console.log(childElements[i]);
-}
-console.log(elementosChilds);
-// elementos.forEach(elemento => {
-//     elemento.addEventListener('mouseover', ()=>console.log(elemento))
-// });
+ function addDetails(tech){
+    console.log('teste');
+    const detail = details[tech.toLowerCase()]
+    if(detail){
+        const containerDetail = document.querySelector('.tec-details')
+        const techTitle = document.querySelector('.nome-tec-titulo')
+        containerDetail.innerHTML=detail
+        techTitle.innerHTML=tech
+        addClass(containerDetail)
+    }
+ }
+
+ document.querySelectorAll('.tec').forEach(element =>{
+    element.addEventListener('mouseover', (event)=>{
+        const tech = event.currentTarget.querySelector('.nome-tec').innerHTML
+        addDetails(tech)
+    })
+ })
