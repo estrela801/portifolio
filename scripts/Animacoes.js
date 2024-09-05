9395e8c2347c70de8c033d445346a17d27893b06
estrela801 <rohenri0808@gmail.com>
2024-09-05 13:35:16 -0300

    feat: adição de novas tecnologias

--- scripts/Animacoes.js
+++ scripts/Animacoes.js
@@ -8,8 +8,10 @@ const details = {
     express: "Express é um framework para Node.js que simplifica a criação de aplicações web e APIs. Ele fornece uma série de ferramentas robustas para lidar com rotas, middleware e gerenciamento de requisições HTTP.",
     mysql: "MySQL é um sistema de gerenciamento de banco de dados relacional de código aberto. Ele é amplamente utilizado para armazenar, gerenciar e recuperar dados em aplicações web e empresariais.",
     figma: "Figma é uma ferramenta de design gráfico baseada em nuvem usada para criar interfaces de usuário, protótipos e colaboração em tempo real.",
-    python: "Python é uma linguagem de programação versátil e popular, conhecida por sua simplicidade e legibilidade, amplamente usada em desenvolvimento web, ciência de dados, inteligência artificial e automação."
+    python: "Python é uma linguagem de programação versátil e popular, conhecida por sua simplicidade e legibilidade, amplamente usada em desenvolvimento web, ciência de dados, inteligência artificial e automação.",
+    typescript: "TypeScript é um superset do JavaScript que adiciona tipagem estática e outros recursos avançados ao JavaScript, ajudando a evitar erros e melhorar a manutenção e escalabilidade de grandes aplicações."
 };
+
 
 const userWindowWidth = window.innerWidth
 const containerInfo = document.querySelector('.details-tec')
